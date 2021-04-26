5123067c49ab32b0302d771e08793fa7d34e9fc3
unknown <paulclue20@gmail.com>
2021-04-26 13:12:52 -0500

    update readme

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,7 +15,7 @@ class CategoriesController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_category
-    @categpry = Category.find(Article.find(6).category_id)
+    @categpry = Category.find(params[:id])
   end
 
 end
