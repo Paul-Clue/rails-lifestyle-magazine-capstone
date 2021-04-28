8391f2d9bd3453421cc3bd8b571cff433ec54612
unknown <paulclue20@gmail.com>
2021-04-28 17:34:13 -0500

    Update article view

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,11 +1,3 @@
 module ArticlesHelper
-  def display_image
-    @img = ''
-    return unless @article.image.attached?
-
-    @img += "<div style='z-index: 0; display: flex; justify-content: center;'>"
-    @img += "<img src=#{url_for(@article.image)} style='width: 20%; z-index: 0;'>"
-    @img += '</div>'
-    @img.html_safe
-  end
+  
 end
