259d702cc33505dd2ca4b2852f83c695bc146206
unknown <paulclue20@gmail.com>
2021-04-28 13:00:50 -0500

    Update app per code review spec

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def logout_button
+  def logout_button(tes_id, for_id, gm_id, luc_id)
     @str = ''
     if logged_in?
       @str += "<li class='nav-item'>"
@@ -9,6 +9,24 @@ module ApplicationHelper
       @str += "  #{link_to 'Write An Article', new_article_path, style: 'color: grey; text-decoration: none;',
                                                                  class: 'css_class'}"
       @str += '</li>'
+      @str += "<li class='nav-item'>"
+      @str += link_to 'Tesla', category_path(tes_id), style: 'color: grey; text-decoration: none;'.to_s
+      @str += '</li>'
+      @str += "<li class='nav-item'>"
+      @str += link_to 'Ford', category_path(ford_id), style: 'color: grey; text-decoration: none;'.to_s
+      @str += '</li>'
+      @str += "<li class='nav-item'>"
+      @str += link_to 'GM', category_path(gm_id), style: 'color: grey; text-decoration: none;'.to_s
+      @str += '</li>'
+      @str += "<li class='nav-item'>"
+      @str += link_to 'Lucid', category_path(luc_id), style: 'color: grey; text-decoration: none;'.to_s
+      @str += '</li>'
+      @str += "&nbsp;"
+      @str += "&nbsp;"
+      @str += "&nbsp;"
+      @str += "&nbsp;"
+      @str += "&nbsp;"
+      @str += "&nbsp;"
       @str += " <li class='nav-item'>"
       @str += "  #{link_to('LogOut', '/logout', method: :get, style: 'color: grey; text-decoration: none;')}"
       @str += '</li>'
