949b5fa5c57d8fb80832dcc325d9e0b243f6f85e
unknown <paulclue20@gmail.com>
2021-04-29 06:41:21 -0500

    Correct liner issues

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,15 +2,15 @@ module ArticlesHelper
   def tse(art)
     @pet_e = ''
     if art.errors.any?
-      @pet_e +=  "<div id='error_explanation'>"
-      @pet_e +=  "    <h2><%= pluralize(article.errors.count, 'error') %> prohibited this article from being saved:</h2>"
-    
-      @pet_e +=  "    <ul>"
-      @pet_e +=  "       <%=article.errors.each do |error|%>"
-      @pet_e +=  "        <li><%= error.full_message</li>"
-      @pet_e +=  "      <%= end %>"
-      @pet_e +=  "    </ul>"
-      @pet_e +=  "</div>"
+      @pet_e += "<div id='error_explanation'>"
+      @pet_e += "    <h2><%= pluralize(article.errors.count, 'error') %> prohibited this article from being saved:</h2>"
+
+      @pet_e += '    <ul>'
+      @pet_e += '       <%=article.errors.each do |error|%>'
+      @pet_e += '        <li><%= error.full_message</li>'
+      @pet_e += '      <%= end %>'
+      @pet_e += '    </ul>'
+      @pet_e += '</div>'
     end
     @pet_e.html_safe
   end
